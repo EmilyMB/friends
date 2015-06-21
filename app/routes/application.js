530cec2e10e290bd1f0061d123bf801b4cc294b2
Emily Berkeley <eberkeley@gmail.com>
2015-06-21 11:47:16 -0400

    Add fontello and picnic and update styles

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
     save: function() {
       return true;
     },
+
     cancel: function() {
       return true;
     }
--- app/routes/friends.js
+++ app/routes/friends.js
@@ -5,9 +5,11 @@ export default Ember.Route.extend({
     save: function() {
       return true;
     },
+
     cancel: function() {
       return true;
     },
+
     delete: function(friend) {
       friend.destroyRecord().then(() => {
         this.transitionTo('friends.index');
