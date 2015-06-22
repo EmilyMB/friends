10941b229e1e3d8badaf71f80117533ee12bbfb9
Emily Berkeley <eberkeley@gmail.com>
2015-06-22 07:55:04 -0400

    Add article resource

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ let Router = Ember.Router.extend({
 Router.map(function() {
   this.route('friends', function() {
     this.route('new');
-    this.route('show', { path: ':friend_id' });
+    this.route('show', { path: ':friend_id' }, function() {
+      this.route('articles');
+    });
     this.route('edit', { path: ':friend_id/edit' });
   });
 });
