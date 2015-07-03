8391752202c6b4368bc623fa4ad4caee8c72cd3f
Emily Berkeley <eberkeley@gmail.com>
2015-07-03 11:43:37 -0400

    Add basic unit tests for article and friend models

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   showReturned: false,
   filteredResults: Ember.computed('model.state', 'showReturned', function() {
-    if(this.showReturned === true) {
+    if (this.showReturned === true) {
       return this.get('this.model');
     } else {
       const articles = this.get('this.model');
       return articles.filter(function(article) {
         return article.get('state') === 'borrowed';
-      })
+      });
     }
   }),
   queryParams: ['show'],
--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,12 +1,32 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('friend', 'Unit | Model | friend', {
-  // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:article']
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
-  // var store = this.store();
   assert.ok(!!model);
+});
+
+test('fullName joins first and last name', function(assert) {
+  const model = this.subject({firstName: 'Bob', lastName: 'Smith'});
+
+  assert.equal(model.get('fullName'), 'Bob Smith');
+
+  Ember.run(function() {
+    model.set('firstName', 'Heidy');
+  });
+
+  assert.equal(model.get('fullName'), 'Heidy Smith', 'Updates fullName');
+});
+
+test('articles relationship', function(assert) {
+  const klass = this.subject({}).constructor;
+
+  const relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
 });
